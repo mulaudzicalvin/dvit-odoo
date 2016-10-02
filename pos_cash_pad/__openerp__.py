b8f75ad040153a4d131dce21810b3516e3fbc02a
abokerba <mohamedashrafkerba@gmail.com>
2016-10-02 11:28:49 +0200

    [New Module] "so_cash_pad"
    This module has been made to limit payment filed in point of sale to "99999"

---
+++ pos_cash_pad/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Point Of Sale Cash Pad',
+    'summary': 'Point Of Sale Cash Pad',
+    'description': """
+    Point Of Sale Cash Pad
+     """,
+    'version': '8.0.1.0',
+    'category': 'Point of Sale',
+    'author': 'DVIT.me',
+    'website': 'http://dvit.me',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'point_of_sale',
+    ],
+    'conflicts': [],
+    'data': ['templates.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+
+}
