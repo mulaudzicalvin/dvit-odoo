3be5ec2e9c123b6448f8d2fde4cae3ebcaa52667
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-03-06 00:37:24 +0200

    '

---
+++ dvit_project_ebsar/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'DVIT Ebsar',
+    'summary': 'ADVIT Ebsar',
+    'description': """
+    ADVIT Ebsar
+     """,
+    'version': '8.0.0.8',
+    'category': 'Productivity',
+    'author': 'DVIT.me',
+    'website': 'http://dvit.me',
+    'license': 'AGPL-3',
+    'depends': ['project_category',
+                    'project_stage_closed',
+                    'project_task_materials_stock',
+                    ],
+    'data': [
+        'templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ dvit_project_ebsar/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class project_project(models.Model):
+    _inherit = 'project.project'
+
+    order_number = fields.Char(string="رقم الطلب" )
+    date_assigned = fields.Date(string="تاريخ الاسناد")
+    client_name = fields.Char(string="اسم المشترك")
+    client_mobile = fields.Char(string="جوال المشترك")
+    es_number = fields.Char(string="رقم محطة كهرباء المشترك")
+    meter_numbers = fields.Integer(string="العدادات المعتمدة")
+    in_number = fields.Char(string="الرقم الداخلي للمشروع")
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    billable = fields.Boolean(string="مستخلص")
+    date_valuated = fields.Date(string="تاريخ استلام وتقييم مبلغ تكلفة العمل في التقرير" )
+    date_budget_start = fields.Date(string="تاريخ بداية موازنة المو اد والاعمال" )
+    date_budget_end = fields.Date(string="تاريخ نهاية موازنة المواد والاعمال" )
+    date_certificate = fields.Date(string="تاريخ شهادة الإنجاز" )
+    date_sent = fields.Date(string="تاريخ إرسال المعامله" )
+    date_invoiced = fields.Date(string="تاريخ فاتورة رقم المستخلص للتوقيع" )
