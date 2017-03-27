80a4e787f33dc8008280bd27d9610ffc929de9d0
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-03-27 21:31:41 +0200

    '

--- pos_fixes/models/pos_order.py
+++ pos_fixes/models/pos_order.py
@@ -12,29 +12,41 @@ class PosOrder(models.Model):
         account_move_obj = self.env['account.move']
         move_line_obj = self.env['account.move.line']
         for order in self:
-            o_lines = order.lines.filtered(lambda l: l.product_id.type in ['product', 'consu'] and \
-                l.product_id.categ_id.property_valuation == 'real_time')
+
+            ao_lines = order.lines.filtered(lambda l: l.product_id.type in ['product', 'consu']) #and \
+            for ao_line in ao_lines:
+                print('------------POSFIX --- ' + str(ao_line.id) + str(ao_line.name) + ' -----------')
+            o_lines = ao_lines.filtered(lambda l: l.product_id.categ_id.property_valuation == 'real_time')
+            for o_line in o_lines:
+                print('------------POSFIX --- ' + str(o_line.id) + str(o_line.name) + ' -----------')
             if o_lines:
                 company_id = order.company_id.id
                 session = order.session_id
-                move_id = self._create_account_move(session.start_at,
+                move = self._create_account_move(session.start_at,
                                                      order.name,
                                                      int(session.config_id.journal_id.id),
                                                      company_id,
                                                      )
+                print ('------------ POSFIX --move-name - ' + str(move.name) + ' ------------')
 
-                move = account_move_obj.browse(move_id)
-                move.write({'narration':'POSFIX'})
+                move_id = move.id
+                print ('------------ POSFIX --move-id - ' + str(move_id) + ' ------------')
+
+                # move.write({'narration':'POSFIX'})
                 amount_total = order.amount_total
 
                 for o_line in o_lines :
+                    print ('------------ POSFIX --- ' + str(o_line.name) + ' ------------')
                     amount = 0
                     # if o_line.product_id.categ_id.property_valuation == 'real_time':
-                    stkacc = o_line.product_id.categ_id.property_stock_account_output_categ and \
-                        o_line.product_id.categ_id.property_stock_account_output_categ
+                    stkacc = o_line.product_id.categ_id.property_stock_account_output_categ_id and \
+                        o_line.product_id.categ_id.property_stock_account_output_categ_id
                         # cost of goods account cogacc
-                    cogacc = o_line.product_id.categ_id.property_account_expense_categ and \
-                        o_line.product_id.categ_id.property_account_expense_categ
+                    cogacc = o_line.product_id.property_account_expense_id and \
+                        o_line.product_id.property_account_expense_id
+                    if not cogacc:
+                        cogacc = o_line.product_id.categ_id.property_account_expense_categ_id and \
+                            o_line.product_id.categ_id.property_account_expense_categ_id
 
                     amount = o_line.qty * o_line.product_id.standard_price
                     line_vals = {
@@ -83,6 +95,9 @@ class PosOrder(models.Model):
                         daml.update(line_vals)
                         move_line_obj.create( caml)
                         move_line_obj.create( daml)
+
+            else:
+                print('---------- NOLINIESPOSFIX ---------')
 
         super(PosOrder, self).create_picking()
         return True
