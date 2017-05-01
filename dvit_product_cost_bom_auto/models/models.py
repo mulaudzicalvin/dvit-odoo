c42848ea9fcf8b6559a3de8d7454da7e923c63a1
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-05-01 17:13:24 +0200

    '

--- dvit_product_cost_bom_auto/models/models.py
+++ dvit_product_cost_bom_auto/models/models.py
@@ -13,7 +13,7 @@ class MrpBom(models.Model):
             for bom in self:
                 bom_cost = 0.0
                 for line in bom.bom_line_ids:
-            if line.product_id.uom_id == line.product_uom:
+                    if line.product_id.uom_id == line.product_uom:
                         bom_cost += line.product_id.standard_price * line.product_qty
                 self.product_tmpl_id.standard_price = bom_cost
 
