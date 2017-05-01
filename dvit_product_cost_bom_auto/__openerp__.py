eba65a4a46ca6d9f92e9d70a710002f14c11133c
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-05-01 19:53:33 +0200

    '

--- dvit_product_cost_bom_auto/__openerp__.py
+++ dvit_product_cost_bom_auto/__openerp__.py
@@ -11,14 +11,14 @@
 
 2- Auto update BoM's Product cost price from the BoM cost on write
 
-3- Auto update cost of all BoMs that include a product on updating that product's cost 
+3- Auto update cost of all BoMs that include a product on updating that product's cost manually or through purchase orders in case of avg price.
     """,
 
     'author': "DVIT.ME",
     'website': "http://www.dvit.me",
 
     'category': 'Uncategorized',
-    'version': '8.0.0.3',
+    'version': '8.0.0.4',
 
     'depends': ['mrp'],
 
