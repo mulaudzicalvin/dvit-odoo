0b268bf505896f620a988096c94410c9329ecd00
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-05-01 16:50:18 +0200

    '

--- dvit_product_cost_bom_auto/models/models.py
+++ dvit_product_cost_bom_auto/models/models.py
@@ -8,12 +8,14 @@ class MrpBom(models.Model):
 
     @api.multi
     def write(self, vals):
-        res = super(MrpBom, self).write(vals)
-        for bom in self:
-            bom_cost = 0.0
-            for line in bom.bom_line_ids:
-		if line.product_id.uom_id == line.product_uom:
-                    bom_cost += line.product_id.standard_price * line.product_qty
-            self.product_tmpl_id.standard_price = bom_cost
+        for id in self:
+            res = super(MrpBom, self).write(vals)
+            for bom in self:
+                bom_cost = 0.0
+                for line in bom.bom_line_ids:
+            if line.product_id.uom_id == line.product_uom:
+                        bom_cost += line.product_id.standard_price * line.product_qty
+                self.product_tmpl_id.standard_price = bom_cost
 
         return res
+
