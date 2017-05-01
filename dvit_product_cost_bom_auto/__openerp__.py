ee206f2a8a4ede197e2e95a802f1392346dd2e49
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-05-01 14:27:32 +0200

    '

---
+++ dvit_product_cost_bom_auto/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product cost from BoM auto",
+
+    'summary': """
+        Auto set product cost from  BoM 
+        """,
+
+    'description': """
+        Auto set product cost from BoM on update.
+    """,
+
+    'author': "DVIT.ME",
+    'website': "http://www.dvit.me",
+
+    'category': 'Uncategorized',
+    'version': '8.0.0.1',
+
+    'depends': ['mrp'],
+
+    'data': [],
+
+    'demo': [],
+}
