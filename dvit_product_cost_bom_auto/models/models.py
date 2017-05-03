21732516748dc7d85210305b16793dd1a9c83124
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-05-03 14:55:29 +0200

    '

---
+++ dvit_product_cost_bom_auto/models/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class MrpBom(models.Model):
+    _inherit = "mrp.bom"
+
+    @api.multi
+    def write(self, vals):
+        res = super(MrpBom, self).write(vals)
+        for bom in self:
+            bom_cost = 0.0
+            for line in bom.bom_line_ids:
+                #if line.product_id.uom_id == line.product_uom:
+                bom_cost += line.product_id.standard_price * line.product_qty
+            self.product_tmpl_id.standard_price = bom_cost
+        return res
+
+
+class ProdTempl(models.Model):
+    _inherit = "product.template"
+    @api.multi
+    def write(self,vals):
+        res = super(ProdTempl,self).write(vals)
+        for prod in self:
+            # get all bom lines contain this prod
+            bom_line_ids = self.env['mrp.bom.line'].search([('product_id.product_tmpl_id','=',prod.id)])
+
+            for bom_line_id in bom_line_ids:
+                #trigger write on bom to update the cost.
+                bom_line = self.env['mrp.bom.line'].browse(bom_line_id.id)
+                bom = bom_line.bom_id
+                bom.write({'code':bom_line.bom_id.code})
+        return res
