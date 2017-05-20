a6fefd701e1c7c750336d054a4d20f7a5fbc7a3e
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-05-20 15:38:52 +0200

    '

--- wh_contract/models.py
+++ wh_contract/models.py
@@ -5,12 +5,12 @@ from openerp import models, fields, api
 class stock_picking(models.Model):
     _inherit = 'stock.picking'
 
-    contract = fields.Many2one(string="Contract", comodel_name="account.analytic.account")
+    contract_id = fields.Many2one('account.analytic.account','Contract')
 
 
 class account_analytic_account(models.Model):
     _inherit = 'account.analytic.account'
 
-    stock_picking_ids = fields.One2many(comodel_name="stock.picking", inverse_name="contract",
-                                        string="Stock Operations", required=False, )
+    stock_picking_ids = fields.One2many('stock.picking', 'contract_id',
+                                        string="Stock Operations", copy=True )
     
