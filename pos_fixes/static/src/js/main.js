ba69ead6c481ad6169f1379c17496197f61c8672
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-05-29 02:12:17 +0200

    '

--- pos_fixes/static/src/js/main.js
+++ pos_fixes/static/src/js/main.js
@@ -1,46 +1,47 @@
 openerp.pos_fixes = function(instance, local) {
-	var QWeb = instance.web.qweb;
-	var _t = instance.web._t;
-	function pad (str, max) {
-		str = str.toString();
-		return str.length < max ? pad("0" + str, max) : str;
-	}
+    var QWeb = instance.web.qweb;
+    var _t = instance.web._t;
+    function pad (str, max) {
+        str = str.toString();
+        return str.length < max ? pad("0" + str, max) : str;
+    }
 
-	instance.point_of_sale.PaymentScreenWidget.include({
-		update_payment_summary: function(){
-			self2 = this;
-			if(!self2.refreshPaymentLine){
-				self2.refreshPaymentLine = setInterval(function(){
-					
-					var thisInterval = this;
-					var currentOrder = self2.pos.get('selectedOrder');
-					var paidTotal = currentOrder.getPaidTotal();
-					var dueTotal = currentOrder.getTotalTaxIncluded();
-					var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0;
-					var change = paidTotal > dueTotal ? paidTotal - dueTotal : 0;
+    instance.point_of_sale.PaymentScreenWidget.include({
+        update_payment_summary: function(){
+            self2 = this;
+            self = this;
+            if(!self2.refreshPaymentLine){
+                self2.refreshPaymentLine = setInterval(function(){
 
-					$("[class*='paymentline-input']").each(function(index, element){
-						if ($(this).val() > (dueTotal * 100)){
-							$(this).val(0);
-						}
-					});
+                    var thisInterval = this;
+                    var currentOrder = self2.pos.get('selectedOrder');
+                    var paidTotal = currentOrder.getPaidTotal();
+                    var dueTotal = currentOrder.getTotalTaxIncluded();
+                    var remaining = dueTotal > paidTotal ? dueTotal - paidTotal : 0;
+                    var change = paidTotal > dueTotal ? paidTotal - dueTotal : 0;
 
-					self2.$('.payment-due-total').html(self.format_currency(dueTotal));
-					self2.$('.payment-paid-total').html(self.format_currency(paidTotal));
-					self2.$('.payment-remaining').html(self.format_currency(remaining));
-					self2.$('.payment-change').html(self.format_currency(change));
-					if(currentOrder.selected_orderline === undefined){ remaining = 1;  }
-					$("li.button:nth-child(2)").on("click",function(){
-						clearInterval(thisInterval);
-					});
-				} ,15);
-			}
+                    $("[class*='paymentline-input']").each(function(index, element){
+                        if ($(this).val() > (dueTotal * 100)){
+                            $(this).val(0);
+                        }
+                    });
 
-			if(self2.pos_widget.action_bar){
-				self2.pos_widget.action_bar.set_button_disabled('validation', !self2.is_paid());
-				self2.pos_widget.action_bar.set_button_disabled('invoice', !self2.is_paid());
-			}
+                    self2.$('.payment-due-total').html(self.format_currency(dueTotal));
+                    self2.$('.payment-paid-total').html(self.format_currency(paidTotal));
+                    self2.$('.payment-remaining').html(self.format_currency(remaining));
+                    self2.$('.payment-change').html(self.format_currency(change));
+                    if(currentOrder.selected_orderline === undefined){ remaining = 1;  }
+                    $("li.button:nth-child(2)").on("click",function(){
+                        clearInterval(thisInterval);
+                    });
+                } ,15);
+            }
 
-		}
-	});
+            if(self2.pos_widget.action_bar){
+                self2.pos_widget.action_bar.set_button_disabled('validation', !self2.is_paid());
+                self2.pos_widget.action_bar.set_button_disabled('invoice', !self2.is_paid());
+            }
+
+        }
+    });
 };
