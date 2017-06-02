d7c029d1073bf135d509e38fb8d2f187b400bc51
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-06-02 03:34:14 +0200

    '

---
+++ dvit_product_pack_ext/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'IngAdhoc Product Pack extension',
+    'summary': 'Product packs extension',
+    'description': """
+This module is extension for INGADHOC's module product_pack that will
+auto set product type to service if it's a pack.
+     """,
+    'version': '8.0.0.1',
+    'category': 'Product',
+    'author': 'DVIT.me',
+    'website': 'http://dvit.me',
+    'license': 'AGPL-3',
+    'depends': ['product_pack'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
---
+++ dvit_product_pack_ext/models.py
@@ -0,0 +1,19 @@
+from openerp import models, api, fields
+
+class ProdTempl(models.Model):
+    _inherit = 'product.template'
+
+    @api.multi
+    def write(self, vals):
+        if not 'pack' in vals:
+            vals['pack'] = self.pack
+        # print vals
+        if 'pack' in vals and vals['pack'] == True:
+            vals['type'] = 'service'
+        return super(product_template, self).write(vals)
+
+    @api.model
+    def create(self,vals):
+        if vals['pack'] == True:
+            vals['type'] = 'service'
+        return super(product_template, self).create(vals)
