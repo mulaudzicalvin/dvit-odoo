c574c5639da0e05a83295cbc4188f6936ce2d80d
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-06-05 07:00:00 +0200

    '

--- pos_fixes/models/pos_order.py
+++ pos_fixes/models/pos_order.py
@@ -80,73 +80,73 @@ class PosOrder(models.Model):
                             pack_line.product_id.property_account_expense_id or \
                             pack_line.product_id.categ_id.property_account_expense_categ_id
 
-                        # if cogacc and stkacc:
-                        #TODO Check and convert UoM/UoS
-                        amount = line.qty * pack_line.quantity * pack_line.product_id.standard_price
-                        name = str(line.qty) + 'x' + line.product_id.name + ' > ' + \
-                        str(pack_line.quantity * line.qty) + 'x' + pack_line.product_id.name
-                        line_vals = {
-                            'name': name,
-                            'move_id': move.id,
-                            'journal_id': move.journal_id.id,
-                            'date': move.date,
-                            'product_id': pack_line.product_id.id,
-                            'partner_id': order.partner_id and order.partner_id.id or False,
-                            'quantity': pack_line.quantity * line.qty,
-                            'ref': line.name,
-                        }
+                        if cogacc and stkacc:
+                            #TODO Check and convert UoM/UoS
+                            amount = line.qty * pack_line.quantity * pack_line.product_id.standard_price
+                            name = str(line.qty) + 'x' + line.product_id.name + ' > ' + \
+                            str(pack_line.quantity * line.qty) + 'x' + pack_line.product_id.name
+                            line_vals = {
+                                'name': name,
+                                'move_id': move.id,
+                                'journal_id': move.journal_id.id,
+                                'date': move.date,
+                                'product_id': pack_line.product_id.id,
+                                'partner_id': order.partner_id and order.partner_id.id or False,
+                                'quantity': pack_line.quantity * line.qty,
+                                'ref': line.name,
+                            }
 
-                        # create move.lines to credit stock and debit cogs
-                        caml = {
-                            'account_id': stkacc.id,
-                            'credit': amount_total > 0 and amount or 0.0,
-                            'debit': amount_total < 0 and -amount or 0.0,
-                        }
-                        caml.update(line_vals)
-                        daml = {
-                            'account_id': cogacc.id,
-                            'debit': amount_total > 0 and amount or 0.0,
-                            'credit': amount_total < 0 and -amount or 0.0,
-                        }
-                        daml.update(line_vals)
+                            # create move.lines to credit stock and debit cogs
+                            caml = {
+                                'account_id': stkacc.id,
+                                'credit': amount_total > 0 and amount or 0.0,
+                                'debit': amount_total < 0 and -amount or 0.0,
+                            }
+                            caml.update(line_vals)
+                            daml = {
+                                'account_id': cogacc.id,
+                                'debit': amount_total > 0 and amount or 0.0,
+                                'credit': amount_total < 0 and -amount or 0.0,
+                            }
+                            daml.update(line_vals)
 
-                        # #TODO if the pack itself is a service POS will not
-                        # # create stock entries at all for its components so we
-                        # # need to process it
-                        # valacc = pack_line.product_id.categ_id.property_stock_valuation_account_id
-                        # val_journal = self.env['account.journal'].search([('type','=','misc')])
-                        #
-                        # valuation_lines={
-                        # 'name': name,
-                        # 'move_id': move.id,
-                        # 'journal_id': move.journal_id.id,
-                        # 'date': move.date,
-                        # 'product_id': pack_line.product_id.id,
-                        # 'partner_id': order.partner_id and order.partner_id.id or False,
-                        # 'quantity': pack_line.quantity * line.qty,
-                        # 'ref': line.name,
-                        # }
-                        #
-                        # # create stock valuation entries cr. inventory dr. goods shipped not invoiced
-                        # vcaml = {
-                        #     'account_id': valacc.id,
-                        #     'credit': amount_total > 0 and amount or 0.0,
-                        #     'debit': amount_total < 0 and -amount or 0.0,
-                        # }
-                        # vcaml.update(line_vals)
-                        #
-                        # vdaml = {
-                        #     'account_id': stkacc.id,
-                        #     'debit': amount_total > 0 and amount or 0.0,
-                        #     'credit': amount_total < 0 and -amount or 0.0,
-                        # }
-                        # vdaml.update(line_vals)
-                        # # FIXME: Below should go in the stock journal
-                        # move_line_obj.with_context(check_move_validity=False).create(vcaml)
-                        # move_line_obj.with_context(check_move_validity=False).create(vdaml)
+                            # #TODO if the pack itself is a service POS will not
+                            # # create stock entries at all for its components so we
+                            # # need to process it
+                            # valacc = pack_line.product_id.categ_id.property_stock_valuation_account_id
+                            # val_journal = self.env['account.journal'].search([('type','=','misc')])
+                            #
+                            # valuation_lines={
+                            # 'name': name,
+                            # 'move_id': move.id,
+                            # 'journal_id': move.journal_id.id,
+                            # 'date': move.date,
+                            # 'product_id': pack_line.product_id.id,
+                            # 'partner_id': order.partner_id and order.partner_id.id or False,
+                            # 'quantity': pack_line.quantity * line.qty,
+                            # 'ref': line.name,
+                            # }
+                            #
+                            # # create stock valuation entries cr. inventory dr. goods shipped not invoiced
+                            # vcaml = {
+                            #     'account_id': valacc.id,
+                            #     'credit': amount_total > 0 and amount or 0.0,
+                            #     'debit': amount_total < 0 and -amount or 0.0,
+                            # }
+                            # vcaml.update(line_vals)
+                            #
+                            # vdaml = {
+                            #     'account_id': stkacc.id,
+                            #     'debit': amount_total > 0 and amount or 0.0,
+                            #     'credit': amount_total < 0 and -amount or 0.0,
+                            # }
+                            # vdaml.update(line_vals)
+                            # # FIXME: Below should go in the stock journal
+                            # move_line_obj.with_context(check_move_validity=False).create(vcaml)
+                            # move_line_obj.with_context(check_move_validity=False).create(vdaml)
 
-                        move_line_obj.with_context(check_move_validity=False).create(caml)
-                        move_line_obj.with_context(check_move_validity=False).create(daml)
+                            move_line_obj.with_context(check_move_validity=False).create(caml)
+                            move_line_obj.with_context(check_move_validity=False).create(daml)
 
                 move.sudo().post()
 
