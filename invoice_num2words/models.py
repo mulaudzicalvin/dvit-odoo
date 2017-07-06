d3b2b43b35131ff212aedc82a14838378d4d152a
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-07-06 21:08:02 +0200

    '

---
+++ invoice_num2words/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+from num2words import num2words
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+    total_amount_in_words = fields.Char(string="Amount in Words", store=True)
+
+    @api.one
+    @api.depends('invoice_line.price_subtotal', 'tax_line.amount')
+    def _compute_amount(self):
+        res = super(AccountInvoice,self)._compute_amount()
+        supported_langs = ['ar','en','fr','de','dk','ru','he','id','lt','lv','no','pl']
+        lang = self.partner_id and self.partner_id.lang[:2]
+        if not lang in supported_langs:
+            lang = 'en'
+        self.total_amount_in_words = num2words(self.amount_total,lang=lang)
+        # print "-------------------------------"
+        # print self.total_amount_in_words
+        # print "-------------------------------"
+        return res
