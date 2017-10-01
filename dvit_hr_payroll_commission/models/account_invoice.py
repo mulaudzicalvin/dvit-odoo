4d9aad64a0029e8e8b7ce1cd9e8b2e561dfb7e08
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-10-01 14:03:37 +0200

    [New] dvit_hr_payroll_commission ported from v8

--- dvit_hr_payroll_commission/models/account_invoice.py
+++ dvit_hr_payroll_commission/models/account_invoice.py
@@ -11,12 +11,4 @@ class AccountInvoice(models.Model):
 
     slip_ids = fields.Many2many('hr.payslip', string='Commission Payslips')
 
-    @api.multi
-    def invoice_validate(self):
-    	res = super(AccountInvoice, self).invoice_validate()
-        emp_id = self.env['hr.employee'].search([('user_id','=',self.user_id.id)])[0]
-        contracts = self.env['hr.contract'].search([('employee_id','=',emp_id.id)])
-        for contract in contracts:
-        	contract._comp_commission()
-        return res
 
