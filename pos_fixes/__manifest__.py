d07933df5961ba20eb2c2440b1effac5e9110392
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-10-15 03:01:21 +0200

    update summary and price.

--- pos_fixes/__manifest__.py
+++ pos_fixes/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'POS Fixes',
-    'summary': 'Point of Sale Fixes',
+    'summary': 'Point of Sale Fixes: complete stock account moves, mark pos invoices as paid and reconcile them.',
     'description': """
     Fixes included:
 
@@ -8,19 +8,15 @@
 
     - Complete the Gr/Ir anglo-saxon journal entries missing in POS.
 
-    - prevent barcode on payment screen using js events .
-
     - Now supports IngAdhoc's Product packs 1- create anglo-saxon for packs.
 
     ToDo:
-
-    - Reimplement Support & Process sales of product packs by IngAdhoc's product_pack.
 
     - Improve performance by reconciling invoices per order close not in session close .
 
      """,
      "license": "AGPL-3",
-     "price": 25.0,
+     "price": 35.0,
      "currency": 'EUR',
     'version': '10.0.2.0',
     'category': 'Point of Sale',
