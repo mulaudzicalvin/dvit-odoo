23304941320270234639df2ac1d580cf22747d6e
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-10-28 03:09:32 +0200

    Port OCA's mrp_sale_info to 10.0 - show sale info on MRP MO & WO.

--- dvit_mrp_sale_info/models/mrp_production.py
+++ dvit_mrp_sale_info/models/mrp_production.py
@@ -14,7 +14,8 @@ class MrpProduction(models.Model):
     partner_id = fields.Many2one(related='sale_id.partner_id',
                                  string='Customer', store=True)
 
-    @api.depends('move_raw_ids','move_finished_ids')
+    @api.depends('move_raw_ids.state','move_finished_ids.state',
+    'move_raw_ids','move_finished_ids')
     def get_sale_info(self):
         for prod in self:
             if not prod.move_finished_ids:
@@ -22,4 +23,3 @@ class MrpProduction(models.Model):
             prod.update({
             'sale_id': prod.move_finished_ids[0].move_dest_id.picking_id.sale_id,
             })
-            prod.action_assign()
