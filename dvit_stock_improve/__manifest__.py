e69222ce0b5b88e7294b6008422cf1eb4221896d
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-11-03 03:48:19 +0200

    new module

---
+++ dvit_stock_improve/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Warehouse Improvements',
+    'summary': 'Improve warehouse Management',
+    'version': '10.0.1.0',
+    'category': 'Stock',
+    'license': 'AGPL-3',
+    'price': 75.0,
+    'currency': 'EUR',
+    'author': "DVIT.ME",
+    'website': 'http://dvit.me/',
+    'depends': ['sale_stock'],
+    'data': ['views/templates.xml',],
+    'installable': True,
+}
