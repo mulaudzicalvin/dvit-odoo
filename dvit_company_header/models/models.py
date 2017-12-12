1549583404e832c557ce7918fcf11855174bc6cb
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-12-12 05:52:35 +0200

    fix

--- dvit_company_header/models/models.py
+++ dvit_company_header/models/models.py
@@ -5,8 +5,5 @@ from odoo import models, fields, api
 class resCompany(models.Model):
     _inherit = 'res.company'
 
-
-
-    header = fields.Binary("Header")
-    footer = fields.Binary("Footer")
-
+    header = fields.Binary("Header", help="PNG image 21x4 CM at most")
+    footer = fields.Binary("Footer", help="PNG image 21x4 CM at most")
