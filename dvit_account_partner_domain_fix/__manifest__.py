917a4ea4817c7bb6f7053998ddf55ffb34c7d349
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-12-20 16:49:48 +0200

    new mod

---
+++ dvit_account_partner_domain_fix/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Account partner domain fix',
+    'summary': 'Allow selecting all partners in bank statement, cash register, Journal Entries.',
+    'version': '10.0.1.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'author': "DVIT.ME",
+    'website': 'http://dvit.me/',
+    'depends': ['account'],
+    'data': ['views/views.xml'],
+    'installable': True,
+}
