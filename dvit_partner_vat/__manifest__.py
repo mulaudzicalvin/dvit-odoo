e1b7c27079e20f98f413b99fd2d7f4cc2ee74797
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2017-12-20 18:23:38 +0200

    new mod

---
+++ dvit_partner_vat/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Partner VAT/TIN ',
+    'summary': 'Show VAT/TIN field on partners.',
+    'version': '10.0.1.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'author': "DVIT.ME",
+    'website': 'http://dvit.me/',
+    "price": 5.0,
+    "currency": 'EUR',
+    'depends': ['account'],
+    'data': ['views/views.xml'],
+    'installable': True,
+}
