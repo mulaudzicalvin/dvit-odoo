afcc59c71cfd55955c2f116929768c9a04adf3e4
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2018-02-12 18:49:49 +0200

    new

---
+++ dvit_product_unique_default_code/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Unique Product Code",
+    'summary': """Unique Product Code""",
+    'author': "DVIT.ME",
+    'website': "http://dvit.me",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'auto_install': True,
+    'depends': ['product'],
+
+}
---
+++ dvit_product_unique_default_code/models/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    @api.multi
+    def copy(self, default=None):  # pylint: disable=W0622
+
+        if not default:
+            default = {}
+
+        default[
+            'default_code'] = ""
+
+        return super(ProductProduct, self).copy(default=default)
+
+    _sql_constraints = [
+        ('default_code_unique', 'UNIQUE(default_code)', 'Internal Reference already exists !'),
+    ]
