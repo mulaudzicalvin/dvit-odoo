175aea09c8ed4d4a0fafc7e967bfe3ace6152f83
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2018-02-14 23:14:25 +0200

    clean & add support for multi-currency

--- dvit_product_last_purchase_price/models/models.py
+++ dvit_product_last_purchase_price/models/models.py
@@ -3,27 +3,27 @@ from odoo import models, fields, api,_
 from odoo.exceptions import UserError, RedirectWarning, ValidationError
 from datetime import datetime
 
-class dvit_purchase_price(models.Model):
-    # _name = 'dvit_purchase_price.dvit_purchase_price'
+class ProductTemplate(models.Model):
     _inherit = ['product.template']
     last_purchase_price = fields.Float(string="Last purchase price")
 
-class account_invoice(models.Model):
+class AccountInvoice(models.Model):
     _inherit = ['account.invoice']
 
     @api.multi
     def action_invoice_open(self):
-        # lots of duplicate calls to action_invoice_open, so we remove those already open
-        to_open_invoices = self.filtered(lambda inv: inv.state != 'open')
-        if to_open_invoices.filtered(lambda inv: inv.state not in ['proforma2', 'draft']):
-            raise UserError(_("Invoice must be in draft or Pro-forma state in order to validate it."))
-        to_open_invoices.action_date_assign()
-        to_open_invoices.action_move_create()
-        products=self.env['product.product']
-        for line in self.invoice_line_ids:
-            product=products.browse(line.product_id.id)
-            product.product_tmpl_id.last_purchase_price=line.price_unit
-        return to_open_invoices.invoice_validate()
+        for inv in self:
+            for line in inv.invoice_line_ids:
+                #TODO: what about currency ?!!
+                unit_price = line.price_unit
+                company_currency = inv.company_id.currency_id
+                if inv.currency_id != company_currency:
+                    unit_price = inv.currency_id.with_context(
+                    date=inv._get_currency_rate_date()).compute(
+                    abs(unit_price), inv.company_id.currency_id)
+                line.product_id.product_tmpl_id.last_purchase_price = unit_price
+            res = super(AccountInvoice,self).action_invoice_open()
+        return res
 
 class PriceListItem(models.Model):
     _inherit = 'product.pricelist.item'
