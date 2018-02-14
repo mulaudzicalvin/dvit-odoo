a72f80f1f6e366a750affe08414b51b3a4744bed
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2018-02-14 19:24:09 +0200

    fix

--- dvit_product_unique_default_code/models/models.py
+++ dvit_product_unique_default_code/models/models.py
@@ -2,6 +2,25 @@
 
 from odoo import models, fields, api
 
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.multi
+    def copy(self, default=None):  # pylint: disable=W0622
+
+        if not default:
+            default = {}
+
+        default[
+            'default_code'] = ""
+
+        return super(ProductProduct, self).copy(default=default)
+
+    _sql_constraints = [
+        ('default_code_unique', 'UNIQUE(default_code)', 'Internal Reference already exists !'),
+    ]
+
+
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
