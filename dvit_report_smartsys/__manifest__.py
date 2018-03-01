749054adee1d5e821d82a584aae7068039f12e86
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2018-03-01 20:17:35 +0200

    .

--- dvit_report_smartsys/__manifest__.py
+++ dvit_report_smartsys/__manifest__.py
@@ -15,8 +15,6 @@
         'sales_report_product_image',
         'dvit_sale_hide_price',
         'dvit_sale_duplicate_lines',
-       'dvit_sale_option_lines',
-       'dvit_sale_alt_product',
        'dvit_sale_line_no',
        'sale_comment_template',
         ],
