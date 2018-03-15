dc522a2e9cca4179b4b1fe78cec29c64667bd052
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2018-03-15 05:43:06 +0200

    .

--- dvit_hora_address/models/models.py
+++ dvit_hora_address/models/models.py
@@ -2,6 +2,13 @@
 
 from odoo import api, fields, models, _
 
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    date_order = fields.Datetime(related="order_id.date_order", )
+
+
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
