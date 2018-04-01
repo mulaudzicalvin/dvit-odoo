c11aad175c39b34549ef08d558acfe5ba14cb27d
Mohamed M. Hagag <mohamedhagag1981@gmail.com>
2018-04-01 04:49:05 +0200

    FIX product uom_po_id change

---
+++ dvit_product_uom_change_fix/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'FIX: Allow Product UoM Change',
+    'summary': 'Odoo prevents Product UoM change even in same UoM category which is useless and restrictive, this will fix this issue allowing you to change product UoM in the same UoM category only.',
+    'version': '10.0.1.0',
+    'category': 'Product',
+    'license': 'AGPL-3',
+    'author': "DVIT.ME",
+    'website': 'http://dvit.me/',
+    'depends': ['account'],
+    "images": [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
